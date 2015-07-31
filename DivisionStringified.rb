6b6ec221a44eba3e48a2fd506a79b3eae0073f54
Yeojin seo <wordplay.yj@gmail.com>
2015-07-31 11:16:14 +0900

    Create DivisionStringified.rb

---
+++ DivisionStringified.rb
@@ -0,0 +1,7 @@
+def DivisionStringified(num1,num2)
+
+  # code goes here
+  num1 = num1.to_f/num2.to_f 
+  num1 = num1.round 
+  return num1.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse 
+ end 
