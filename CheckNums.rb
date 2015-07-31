0d35138a9b3c7c410e6b4e56b90919a38cf2d4bc
Yeojin seo <wordplay.yj@gmail.com>
2015-07-31 11:07:28 +0900

    Create CheckNums.rb

---
+++ CheckNums.rb
@@ -0,0 +1,15 @@
+def CheckNums(num1,num2)
+
+  # code goes here
+  flag =
+    if num2 > num1
+      "true"
+      elsif num2 < num1
+      "false"
+    else
+      "-1"
+    end
+  
+  return flag
+         
+end
